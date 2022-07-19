daeb6ba2853a238e86017b4f3f5a47d4de23b0c9
krzysztofkucharzyk <krzysztofkucharzyk@gmail.com>
2022-07-19 12:13:08 +0200

    zrobione

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function tablica () {
+    let array = [32,7,13];
+    
+    if(array[0] > 10) {
+        array[0] = 5;
+    }
+
+    if (array[1] > 10) {
+        array[1] = 5;
+    }
+
+    if (array[2] > 10) {
+        array[2] = 5;
+    }
+    
+return array;
+
+}
+
+console.log(tablica());
